5aa6c9eaff0d5dd39493e33df50a66f2eaf35a26
aineon <enia.oneill@gmail.com>
2020-10-06 14:50:28 +0000

    insert row into friends table

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get Username from workspace
@@ -9,7 +10,7 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-
+"""
 try:
     # Run a query
     with connection.cursor() as cursor:
@@ -18,7 +19,27 @@ try:
         result = cursor.fetchall()
         print(result)
 finally:
-    """ close the connection to SQL regardless of whether
-     the above was successful or not
-    """
+    # close the connection to SQL regardless of whether
+    # the above was successful or not
+
+    connection.close()
+
+
+
+try:
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT * FROM Genre;"
+        cursor.execute(sql)
+        for row in cursor:
+            print(row)
+finally:
+    connection.close()
+"""
+
+try:
+    with connection.cursor() as cursor:
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
+finally:
     connection.close()
