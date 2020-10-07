a4fb8f82566d3ccf98c11ca169f08e1b2f49d675
aineon <enia.oneill@gmail.com>
2020-10-07 14:41:37 +0000

    delete many using where in

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -38,7 +38,12 @@ finally:
 
 try:
     with connection.cursor() as cursor:
-        rows = cursor.execute("DELETE FROM Friends WHERE name = %s;", 'Bob')
+        list_of_names = ["Fred", "Fred"]
+        # Prepare a string with the sanme number of placeholders
+        # as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});"
+                       .format(format_strings), list_of_names)
     connection.commit()
 finally:
     connection.close()
